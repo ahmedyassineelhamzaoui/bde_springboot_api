ae036b8728674eb902f5023c910ae200a6b152b4
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2024-04-17 10:31:15 +0100

    create project structure

---
+++ src/main/java/com/example/bde_api/BdeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.bde_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdeApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/bde_api/controllers/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.bde_api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/")
+public class HelloWorld {
+
+    @GetMapping("hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
